e27be59dc5b8bfec2d2bd2ec9bc3505c7a524687
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-02 18:08:11 -0300

    made theme setup

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,20 @@
+
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors: {
-
+    gray: {
+      "700": "#47585B",
+      "500": "#999999",
+      "400": "rgba(153, 153, 153, 0.5)",
+      "300": "#DADADA",
+      "100": "#F5F8FA"
+    },
+    yellow: {
+      "400": "#FFBA08",
+      "300": "rgba(255, 186, 8,0.5)"
+    }
   },
-
   fonts: {
     heading: 'Poppins',
     body: 'Poppins'
@@ -12,9 +22,9 @@ export const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: 'gray.900',
-        color: 'gray.50'
+        bg: 'gray.100',
+        color: 'gray.700'
       }
     }
-  } 
+  }
 })
