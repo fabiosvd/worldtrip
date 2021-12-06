936d9a6bd2c317f82a489e58c6f852eb15a2c77e
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-06 12:06:31 -0300

    finished banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,50 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading, Text, Image } from '@chakra-ui/react';
 
 export default function Banner() {
-  return <Flex></Flex>;
+  return (
+    <Flex
+      w="100%"
+      h={['163px', '264px', '264px', '335px']}
+      bgImage="url('/banner.jpg')"
+      bgPosition={['100% 20%', '100% 20%', '100% 26%']}
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Flex
+        justify={['center', 'space-between']}
+        align="center"
+        w="100%"
+        mx="auto"
+        px={['4', '10', '15', '20', '36']}
+      >
+        <div>
+          <Heading
+            fontSize={['xl', '2xl', '2xl', '2xl', '4xl']}
+            fontWeight="500"
+            color="gray.100"
+          >
+            5 Continentes,
+            <br />
+            infinitas possibilidades.
+          </Heading>
+          <Text
+            maxW={['100%', '100%', '100%', '550px']}
+            fontSize={['0.8rem', 'xl']}
+            color="gray.300"
+            mt="5"
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </div>
+        <Image
+          w={['300px', '300px', '300px', '430px']}
+          display={['none', 'none', 'block']}
+          src="/airplane.svg"
+          alt="Avião voando entre nuvens."
+          ml="8"
+          transform="translateY(55px)"
+        />
+      </Flex>
+    </Flex>
+  );
 }
